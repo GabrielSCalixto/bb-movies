968ba02eabfcdbe3a93af6532191178a4cc3ba67
Gabriel Souza Calixto <gabrielcalixto@192.168.100.3>
2026-05-28 20:46:11 -0300

    Adiciona credenciais Supabase

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  SUPABASE_URL: 'COLE_SUA_URL_AQUI',
-  SUPABASE_ANON_KEY: 'COLE_SUA_ANON_KEY_AQUI',
+  SUPABASE_URL: 'https://nqxbkfpsiyzhdhljavna.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_njSdESNkYaE_SHpXjoo5Uw_gU5tjcCg',
   TMDB_API_KEY: '1bcf451f48d18ea43d1f7c0e2b6b8b63',
 };
